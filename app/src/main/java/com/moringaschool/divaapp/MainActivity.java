6e565ce3eaf8acae82db275c841cbebf5105db62
Diane Ujeneza <dianesis17@gmail.com>
2019-10-11 20:51:42 +0200

    mid commit

--- app/src/main/java/com/moringaschool/divaapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/divaapp/MainActivity.java
@@ -20,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         mFindTracksButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this,TracksActivity.class);
+                Intent intent = new Intent(MainActivity.this,ArtistActivity.class);
                 startActivity(intent);
             }
         });
