36ec9b96940ba684a0c2c1f2fa49147d51f41c2b
Diane Ujeneza <dianesis17@gmail.com>
2019-10-11 18:46:15 +0200

    intents to navigate to another activity page

--- app/src/main/java/com/moringaschool/divaapp/TracksActivity.java
+++ app/src/main/java/com/moringaschool/divaapp/TracksActivity.java
@@ -1,4 +1,14 @@
 package com.moringaschool.divaapp;
 
-public class TracksActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TracksActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tracks);
+    }
 }
