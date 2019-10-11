0f8effc7f626a76ae7baa3a7c94579096102e434
Diane Ujeneza <dianesis17@gmail.com>
2019-10-11 18:36:41 +0200

    intent

--- app/src/main/java/com/moringaschool/divaapp/MainActivity.java
+++ app/src/main/java/com/moringaschool/divaapp/MainActivity.java
@@ -2,13 +2,27 @@ package com.moringaschool.divaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mFindTracksButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mFindTracksButton=(Button) findViewById(R.id.findTracksBtn);
+
+        mFindTracksButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,TracksActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
