1d559a6afbb0119953196ee0ecf3b89d03ca6e04
Diane Ujeneza <dianesis17@gmail.com>
2019-10-11 19:53:17 +0200

    adapters

--- app/src/main/java/com/moringaschool/divaapp/TracksActivity.java
+++ app/src/main/java/com/moringaschool/divaapp/TracksActivity.java
@@ -3,12 +3,23 @@ package com.moringaschool.divaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class TracksActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] tracks = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracks);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_dropdown_item_1line,tracks);
+        mListView.setAdapter(adapter);
     }
 }
