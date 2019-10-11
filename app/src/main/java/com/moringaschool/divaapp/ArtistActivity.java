fde7d0c4a9eea429f0c09099fc9ab49745236846
Diane Ujeneza <dianesis17@gmail.com>
2019-10-11 20:28:46 +0200

    adding a new list

--- app/src/main/java/com/moringaschool/divaapp/ArtistActivity.java
+++ app/src/main/java/com/moringaschool/divaapp/ArtistActivity.java
@@ -3,12 +3,23 @@ package com.moringaschool.divaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ArtistActivity extends AppCompatActivity {
+    private ListView mListView;
+    private String[] artists = new String[] {"Dadju","Chris Brown","Justin Bieber","Ed Sheeran",
+    "Beyonce","Taylor Swift"};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_artist);
+
+        mListView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artists);
+        mListView.setAdapter(adapter);
     }
 }
