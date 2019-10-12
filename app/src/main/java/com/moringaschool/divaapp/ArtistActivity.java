e2dc7f35ecf46feb078ae2ff7ea0f51df597e298
Diane Ujeneza <dianesis17@gmail.com>
2019-10-13 02:50:48 +0300

    backgrounds and on click listeners

--- app/src/main/java/com/moringaschool/divaapp/ArtistActivity.java
+++ app/src/main/java/com/moringaschool/divaapp/ArtistActivity.java
@@ -2,9 +2,15 @@ package com.moringaschool.divaapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ArtistActivity extends AppCompatActivity {
     private ListView mListView;
@@ -21,5 +27,14 @@ public class ArtistActivity extends AppCompatActivity {
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, artists);
         mListView.setAdapter(adapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent =  new Intent(ArtistActivity.this, TracksActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
