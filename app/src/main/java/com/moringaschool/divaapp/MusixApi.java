bae3e6218923af15ff76f558d7a7a146c7f3e8a3
Diane Ujeneza <dianesis17@gmail.com>
2019-10-18 20:24:43 +0200

    added gson dependencies

--- app/src/main/java/com/moringaschool/divaapp/MusixApi.java
+++ app/src/main/java/com/moringaschool/divaapp/MusixApi.java
@@ -1,4 +1,10 @@
 package com.moringaschool.divaapp;
 
+import android.telecom.Call;
+
 public interface MusixApi {
+    @GET("track.search")
+    Call<TrackSearchResponse>getTracks(
+            @Query("track") String tr
+    )
 }
