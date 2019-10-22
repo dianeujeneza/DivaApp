dabd9be7b8a726420bd9259676147e90a28a3586
Diane Ujeneza <dianesis17@gmail.com>
2019-10-22 09:11:35 +0200

    new api

--- app/src/main/java/com/moringaschool/divaapp/network/YelpApi.java
+++ app/src/main/java/com/moringaschool/divaapp/network/YelpApi.java
@@ -1,25 +1,19 @@
 package com.moringaschool.divaapp.network;
 
-import com.moringaschool.divaapp.models.Body;
-import com.moringaschool.divaapp.models.Track;
-import com.moringaschool.divaapp.models.TrackList;
-import com.moringaschool.divaapp.models.TrackSearchResponse;
+import com.moringaschool.divaapp.YelpBusinessSearchResponse;
+//import com.moringaschool.divaapp.models.Body;
+//import com.moringaschool.divaapp.models.Track;
+//import com.moringaschool.divaapp.models.TrackList;
+//import com.moringaschool.divaapp.models.TrackSearchResponse;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface YelpApi {
-    @GET("track.search")
-    Call<TrackList>getTracks(
-//            @Query("track_name") String track_name
-//            @Query("artist_name") String artist_name,
-//            @Query("format") String format,
-//            @Query("callback") String callback,
-//            @Query("apikey") String api,
-            @Query("q_track") String q_track
-//            @Query("q_artist") String q_artist,
-//            @Query("q_lyrics") String q_lyrics
-//            @Query("s_track_rating") double s_track_rating
+    @GET("businesses/search")
+    Call<YelpBusinessSearchResponse> getRestaurants(
+            @Query("location") String location,
+            @Query("term") String term
     );
 }
