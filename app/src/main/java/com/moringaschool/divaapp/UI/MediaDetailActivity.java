db9636f50c8b41945b1c635d64fbd2bf84c5a8d6
Diane Ujeneza <dianesis17@gmail.com>
2019-10-23 19:38:25 +0200

    added an activity to hold medias details

---
+++ app/src/main/java/com/moringaschool/divaapp/UI/MediaDetailActivity.java
@@ -0,0 +1,16 @@
+package com.moringaschool.divaapp.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moringaschool.divaapp.R;
+
+public class MediaDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_media_detail);
+    }
+}
