b7cda207e30503e37938c36aac32f1558691da2a
Diane Ujeneza <dianesis17@gmail.com>
2019-10-26 23:13:01 +0300

    declared variables in create account page

--- app/src/main/java/com/moringaschool/divaapp/UI/CreateAccountActivity.java
+++ app/src/main/java/com/moringaschool/divaapp/UI/CreateAccountActivity.java
@@ -3,14 +3,30 @@ package com.moringaschool.divaapp.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.moringaschool.divaapp.R;
 
-public class CreateAccountActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class CreateAccountActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.createUserButton)
+    Button mCreateUserButton;
+    @BindView(R.id.nameEditText)
+    EditText mNameEditText;
+    @BindView(R.id.emailEditText)
+    TextView mEmailEditText;
+    @BindView(R.id.passwordEditText) TextView mPasswprdEditText;
+    @BindView(R.id.confirmPasswordEditText)TextView mConfirmPasswordEditiText;
+    @BindView(R.id.loginTextView) TextView mLoginTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+
     }
 }
